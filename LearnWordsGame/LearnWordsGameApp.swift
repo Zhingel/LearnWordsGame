2824a1ff16471d8853a482b24fa3929d27c0387e
Stas <staszhingel@icloud.com>
2021-10-05 11:18:27 +0300

    Initial Commit

---
+++ LearnWordsGame/LearnWordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnWordsGameApp.swift
+//  LearnWordsGame
+//
+//  Created by Стас Жингель on 05.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct LearnWordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
